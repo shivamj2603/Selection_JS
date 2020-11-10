884418b52f491ce4b6bf69108664abf1209eb74a
shivamj2603 <shivamj2603@gmail.com>
2020-11-11 05:11:17 +0530

    Added:Program to check whether given year is leap year or not

---
+++ selection3.js
@@ -0,0 +1,11 @@
+var year = process.argv[2];
+var isLeapYear = false;
+if (year % 4 == 0 && year % 100 != 0) {
+    isLeapYear = true;
+}
+else if (year % 100 == 0) {
+    if (year % 400 == 0) {
+        isLeapYear = true;
+    }
+}
+console.log(isLeapYear);
